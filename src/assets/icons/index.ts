9ef8f3f021d23c1e9970057af7080db3633630ff
Fred Leal <flm_@msn.com>
2025-09-23 12:40:44 -0300

    feat: create centralized icon exports
    - Export all icon components from single file
    - Organize by category (ecommerce, navigation, actions)
    - Export BaseIcon types and utilities
    - Enables clean imports: import { CartIcon, HeartIcon } from '@/assets/icons'

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,17 @@
+// src/assets/icons/index.ts
+// Centralized icon exports for easy importing
+
+// E-commerce Icons
+export { CartIcon } from './ecommerce/CartIcon'
+export { HeartIcon } from './ecommerce/HeartIcon'
+
+// Navigation Icons  
+export { SearchIcon } from './navigation/SearchIcon'
+export { MenuIcon } from './navigation/MenuIcon'
+
+// Action Icons
+export { PlusIcon } from './actions/PlusIcon'
+
+// Base types and utilities
+export type { BaseIconProps } from './base/BaseIcon'
+export { iconSizes, iconColors } from './base/BaseIcon'
