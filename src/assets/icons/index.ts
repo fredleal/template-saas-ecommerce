9661b587e4a17d64a00c7681a39cd010e5c7fb8b
Fred Leal <flm_@msn.com>
2025-09-25 18:20:25 -0300

    Add UserIcon to icon system exports
    - Export UserIcon from social category
    - Maintains consistent icon organization
    - Ready for use in components and showcase

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -15,6 +15,9 @@ export { PlusIcon } from './actions/PlusIcon'
 // Feedback Icons
 export { StarIcon } from './feedback/StarIcon'
 
+// Social Icons
+export { UserIcon } from './social/UserIcon'
+
 // Base types and utilities
 export type { BaseIconProps } from './base/BaseIcon'
 export { iconSizes, iconColors } from './base/BaseIcon'
